cfa2ef6010638b64828b38e77ed8a3ea54c11b38
Amaan Shaikh <89447319+Apollo532@users.noreply.github.com>
2024-08-26 17:21:54 +0530

    Add files via upload

---
+++ SQL/Day 5 Hands-On SalesDB.sql
@@ -0,0 +1,219 @@
+create database subtotal;
+
+use subtotal;
+
+CREATE TABLE
+SalesList
+(SalesMonth NVARCHAR(20), SalesQuartes  VARCHAR(5), SalesYear  SMALLINT, SalesTotal MONEY)
+GO
+INSERT INTO  SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'March','Q1',2019,60)
+INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'March','Q1',2020,50)
+INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'May','Q2',2019,30)
+INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'July','Q3',2020,10)
+INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'November','Q4',2019,120)
+INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'October','Q4',2019,150)
+INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'November','Q4',2019,180)
+INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'November','Q4',2020,120)
+INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'July','Q3',2019,160)
+INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'March','Q1',2020,170)
+GO
+SELECT  * FROM SalesList
+
+SELECT  SalesYear, SUM(SalesTotal) AS SalesTotal FROM SalesList
+GROUP BY ROLLUP(SalesYear); /* null is the grand total*/ 
+
+
+SELECT  SalesYear,SalesQuartes, SUM(SalesTotal) AS SalesTotal
+FROM SalesList GROUP BY ROLLUP(SalesYear, SalesQuartes); /* null is the grand total*/ 
+
+
+SELECT SalesMonth, SalesQuartes,SalesYear, SUM(SalesTotal) AS TotalSales
+FROM SalesList
+GROUP BY ROLLUP(SalesMonth, SalesQuartes, SalesYear);
+
+
+SELECT SalesYear, SalesQuartes, SUM(SalesTotal) AS SalesTotal ,
+	GROUPING(SalesQuartes) AS SalesQuarterGrp,
+	GROUPING(SalesYear) AS SYearGrp
+FROM SalesList
+GROUP BY ROLLUP(SalesYear, SalesQuartes);
+
+
+SELECT SalesMonth,SalesTotal , 
+ROW_NUMBER() OVER(ORDER BY NEWID()) AS RowNumber FROM SalesList;
+
+---------- NEW DATABASE ------------------------------------------------------------------
+CREATE DATABASE SalesAnalyticsDB;
+
+
+USE SalesAnalyticsDB;
+
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    email VARCHAR(100),
+    phone_number VARCHAR(15),
+    address VARCHAR(255)
+);
+
+
+CREATE TABLE Products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10, 2),
+    stock_quantity INT
+);
+
+
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+
+CREATE TABLE Sales (
+    sale_id INT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+
+INSERT INTO Customers (customer_id, customer_name, email, phone_number, address) VALUES
+(1, 'John Doe', 'john.doe@example.com', '123-456-7890', '123 Elm Street'),
+(2, 'Jane Smith', 'jane.smith@example.com', '987-654-3210', '456 Oak Avenue'),
+(3, 'Alice Johnson', 'alice.j@example.com', '555-123-4567', '789 Pine Road'),
+(4, 'Bob Brown', 'bob.brown@example.com', '444-987-6543', '321 Maple Boulevard');
+
+
+INSERT INTO Products (product_id, product_name, category, price, stock_quantity) VALUES
+(1, 'Laptop', 'Electronics', 1200.00, 10),
+(2, 'Smartphone', 'Electronics', 800.00, 25),
+(3, 'Tablet', 'Electronics', 400.00, 15),
+(4, 'Headphones', 'Accessories', 100.00, 50),
+(5, 'Monitor', 'Electronics', 300.00, 20);
+
+
+INSERT INTO Orders (order_id, customer_id, order_date) VALUES
+(1, 1, '2024-07-01'),
+(2, 2, '2024-07-02'),
+(3, 1, '2024-07-03'),
+(4, 3, '2024-07-04'),
+(5, 4, '2024-07-05');
+
+
+INSERT INTO Sales (sale_id, order_id, product_id, quantity) VALUES
+(1, 1, 1, 2),
+(2, 2, 2, 1),
+(3, 3, 3, 3),
+(4, 4, 4, 4),
+(5, 5, 1, 1),
+(6, 1, 5, 1),
+(7, 2, 4, 2),
+(8, 3, 2, 2);
+
+
+--Total Amount for each Sale
+
+SELECT S.sale_id, S.order_id, S.product_id, S.quantity, P.price * S.quantity AS SalesTotal
+FROM Sales S
+JOIN Products P ON S.product_id = P.product_id;
+
+--Ranking functions
+
+SELECT product_id, SUM(quantity) AS units_sold,
+	RANK() OVER(ORDER BY SUM(quantity) DESC) AS Salesrank
+FROM Sales
+GROUP BY(product_id);
+
+SELECT product_id, SUM(quantity) AS units_sold,
+	DENSE_RANK() OVER(ORDER BY SUM(quantity) DESC) AS Salesrank
+FROM Sales
+GROUP BY(product_id);
+
+SELECT product_id, SUM(quantity) AS units_sold,
+	ROW_NUMBER() OVER(ORDER BY SUM(quantity) DESC) AS Rownum
+FROM Sales
+GROUP BY(product_id);
+
+SELECT product_id, SUM(quantity) AS units_sold,
+	NTILE(3) OVER(ORDER BY SUM(quantity) DESC) AS Quartile
+FROM Sales
+GROUP BY(product_id);
+
+----CTEs
+
+WITH CustomerSales AS (
+    SELECT O.customer_id, SUM(S.quantity * P.price) AS total_spent
+    FROM Sales S
+    JOIN Orders O ON S.order_id = O.order_id
+    JOIN Products P ON S.product_id = P.product_id
+    GROUP BY O.customer_id
+)
+SELECT * FROM CustomerSales WHERE total_spent > 1000;
+
+
+---OVER() and PARTITION BY
+
+--Running total of sales by product
+
+SELECT product_id, order_id, quantity, 
+	SUM(quantity) OVER(PARTITION BY product_id ORDER BY order_id) AS Quant_R_Total
+FROM Sales;
+
+
+--Average Sale quantity per order by Customer
+SELECT O.customer_id, S.order_id, S.quantity,
+	AVG(quantity) OVER(PARTITION BY customer_id) AS Avg_Q_perCustomer
+FROM Sales S
+JOIN Orders O ON S.order_id = O.order_id;
+
+
+
+---ROLLUP and CUBE
+SELECT category, SUM(price * stock_quantity) AS inventory_value, GROUPING(category) AS Total
+FROM Products
+GROUP BY ROLLUP(category);
+
+
+SELECT category, SUM(price * stock_quantity) AS inventory_value, GROUPING(category) AS Total
+FROM Products
+GROUP BY CUBE(category);
+
+
+---Correlated Subquery
+
+-- Customers who have ordered a specific product
+SELECT customer_name
+FROM Customers C
+WHERE EXISTS (
+	SELECT 1
+	FROM Orders O
+	JOIN Sales S ON O.order_id = S.order_id
+	JOIN Products P ON S.product_id = P.product_id
+	WHERE P.product_name LIKE 'Laptop' AND O.customer_id = C.customer_id);
+
+
+---PROCEDURE - that calculates and returns the total sale by Customer
+
+CREATE PROCEDURE SalesbyCustomer 
+	@CustId INT
+AS
+BEGIN
+SELECT C.customer_name, SUM(S.quantity*P.Price) AS SalesValue
+FROM Customers C
+JOIN Orders O ON C.customer_id = O.order_id
+JOIN Sales S ON O.order_id = S.order_id
+JOIN Products P ON S.product_id = P.product_id
+WHERE C.customer_id = @CustId
+GROUP BY C.customer_name;
+END;
+
+EXEC SalesbyCustomer @CustId = 3;
+
---
+++ SQL/Day 5 MoviesDB & Recursive CTE Practice.sql
@@ -0,0 +1,217 @@
+
+CREATE DATABASE MoviesDB;
+
+USE MoviesDB;
+
+
+
+CREATE TABLE Movies (
+    movie_id INT PRIMARY KEY,
+    title VARCHAR(100),
+    release_year INT,
+    genre VARCHAR(50),
+    box_office_collection DECIMAL(15, 2),
+    director_id INT,
+    FOREIGN KEY (director_id) REFERENCES Directors(director_id)
+);
+
+
+CREATE TABLE Actors (
+    actor_id INT PRIMARY KEY,
+    actor_name VARCHAR(100),
+    birthdate DATE,
+    nationality VARCHAR(50)
+);
+
+
+CREATE TABLE Directors (
+    director_id INT PRIMARY KEY,
+    director_name VARCHAR(100),
+    birthdate DATE,
+    nationality VARCHAR(50)
+);
+
+
+CREATE TABLE Ratings (
+    rating_id INT PRIMARY KEY,
+    movie_id INT,
+    rating_source VARCHAR(50),
+    rating DECIMAL(3, 1),
+    FOREIGN KEY (movie_id) REFERENCES Movies(movie_id)
+);
+
+
+INSERT INTO Directors (director_id, director_name, birthdate, nationality) VALUES
+(1, 'Christopher Nolan', '1970-07-30', 'British-American'),
+(2, 'Quentin Tarantino', '1963-03-27', 'American'),
+(3, 'Steven Spielberg', '1946-12-18', 'American');
+
+
+INSERT INTO Movies (movie_id, title, release_year, genre, box_office_collection, director_id) VALUES
+(1, 'Inception', 2010, 'Sci-Fi', 829895144.00, 1),
+(2, 'Pulp Fiction', 1994, 'Crime', 213928762.00, 2),
+(3, 'Jurassic Park', 1993, 'Adventure', 1039179154.00, 3),
+(4, 'Interstellar', 2014, 'Sci-Fi', 701729206.00, 1),
+(5, 'Django Unchained', 2012, 'Western', 425368238.00, 2);
+
+INSERT INTO Movies VALUES(6, 'Inglorious Bastards', 2010, 'Cri', 2765000890.00,2);
+
+
+INSERT INTO Actors (actor_id, actor_name, birthdate, nationality) VALUES
+(1, 'Leonardo DiCaprio', '1974-11-11', 'American'),
+(2, 'Samuel L. Jackson', '1948-12-21', 'American'),
+(3, 'Tom Hanks', '1956-07-09', 'American'),
+(4, 'Matthew McConaughey', '1969-11-04', 'American'),
+(5, 'Jamie Foxx', '1967-12-13', 'American');
+
+
+INSERT INTO Ratings (rating_id, movie_id, rating_source, rating) VALUES
+(1, 1, 'IMDB', 8.8),
+(2, 1, 'Rotten Tomatoes', 87.0),
+(3, 2, 'IMDB', 8.9),
+(4, 2, 'Rotten Tomatoes', 92.0),
+(5, 3, 'IMDB', 8.1),
+(6, 3, 'Rotten Tomatoes', 91.0),
+(7, 4, 'IMDB', 8.6),
+(8, 4, 'Rotten Tomatoes', 72.0),
+(9, 5, 'IMDB', 8.4),
+(10, 5, 'Rotten Tomatoes', 87.0);
+
+
+--Cleansing and Manipulation
+
+UPDATE Movies
+SET title = LTRIM(RTRIM(title));
+
+UPDATE Ratings
+SET rating = (SELECT AVG(rating) FROM Ratings WHERE rating IS NOT NULL)
+WHERE rating IS NULL;
+
+UPDATE Movies
+SET genre = 'Crime'
+WHERE genre IN (SELECT DISTINCT(genre) FROM Movies WHERE genre LIKE 'Cr%');
+
+-- Regex
+
+SELECT title
+FROM Movies 
+WHERE title LIKE 'In%';
+
+SELECT director_name
+FROM Directors
+WHERE director_name LIKE 'Q% Tar%';
+
+--Analytical Function
+
+--Running Total of BoxOfficeCollection
+SELECT title, genre, box_office_collection, 
+       SUM(box_office_collection) OVER ( PARTITION BY genre ORDER BY release_year) AS RunningTotal
+FROM Movies;
+
+--Ranking functions
+--RANK()
+
+SELECT title, genre, box_office_collection,
+	RANK() OVER(ORDER BY box_office_collection DESC) AS MovieRank
+FROM Movies;
+
+--DENSE_RANK()
+SELECT title, genre, box_office_collection,
+	DENSE_RANK() OVER(ORDER BY box_office_collection DESC) AS MovieRank
+FROM Movies;
+
+--ROW_NUMBER()
+
+SELECT title, genre, box_office_collection,
+	ROW_NUMBER() OVER(ORDER BY box_office_collection DESC) AS MovieRank
+FROM Movies;
+
+
+--NTILE()
+
+SELECT title, genre, box_office_collection,
+	NTILE(3) OVER(ORDER BY box_office_collection DESC) AS Quartiles
+FROM Movies;
+
+
+--CTE Queries
+
+WITH AvgRatings AS (
+SELECT movie_id, AVG(rating) AS AvgRating
+FROM Ratings
+GROUP BY movie_id
+)
+SELECT M.title, AR.AvgRating
+FROM Movies M
+JOIN AvgRatings AR ON M.movie_id = AR.movie_id;
+
+
+-- CUBE and ROLLUP
+
+SELECT director_id, genre, SUM(box_office_collection) AS Collections
+FROM Movies
+GROUP BY ROLLUP(genre, director_id);
+
+
+SELECT director_id, genre, SUM(box_office_collection) AS Collections
+FROM Movies
+GROUP BY CUBE(genre, director_id);
+
+
+
+--Correlation Query
+SELECT title, box_office_collection
+FROM Movies
+WHERE box_office_collection > (SELECT AVG(box_office_collection) FROM Movies);
+
+--PROCEDURE
+
+CREATE PROCEDURE GetMoviesByGenre
+    @Genre VARCHAR(50)
+AS
+BEGIN
+    SELECT movie_id, title, release_year, box_office_collection
+    FROM Movies
+    WHERE genre = @Genre;
+END;
+
+EXEC GetMoviesByGenre @Genre = 'Crime';
+
+--- For Hierarchical Query (Recursive CTE):
+
+CREATE DATABASE OrgChartDB;
+GO
+USE OrgChartDB;
+GO
+
+
+CREATE TABLE Employees (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(100),
+    manager_id INT NULL
+);
+
+
+INSERT INTO Employees (employee_id, employee_name, manager_id) VALUES
+(1, 'CEO', NULL),            -- CEO has no manager
+(2, 'CTO', 1),               -- CTO reports to CEO
+(3, 'CFO', 1),               -- CFO reports to CEO
+(4, 'Engineering Manager', 2), -- Engineering Manager reports to CTO
+(5, 'Finance Manager', 3),   -- Finance Manager reports to CFO
+(6, 'Software Engineer', 4), -- Software Engineer reports to Engineering Manager
+(7, 'Accountant', 5);        -- Accountant reports to Finance Manager
+
+
+WITH EmpHierarchy AS (
+SELECT employee_id, employee_name, manager_id, 1 AS level
+FROM Employees
+WHERE manager_id IS NULL
+UNION ALL
+SELECT E.employee_id, E.employee_name, E.manager_id, EH.level + 1 AS level
+FROM Employees E
+INNER JOIN EmpHierarchy EH ON E.manager_id = EH.employee_id
+)
+
+SELECT employee_id, employee_name, manager_id, level
+FROM EmpHierarchy
+ORDER BY level,manager_id, employee_id;
